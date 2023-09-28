8364f0104f0e6209a2cba2fec25ea8b166484d70
Orrv2904 <Omarrv2904@gmail.com>
2023-09-28 15:20:33 -0600

    :tada:

--- qr.py
+++ qr.py
@@ -1,9 +1,7 @@
 import qrcode
 
-# Define el enlace que deseas codificar en el QR
 enlace = "https://github.com/Orrv2904"
 
-# Crea un objeto QRCode
 codigo_qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -11,14 +9,10 @@ codigo_qr = qrcode.QRCode(
     border=4,
 )
 
-# Agrega el enlace al código QR
 codigo_qr.add_data(enlace)
 codigo_qr.make(fit=True)
 
-# Crea una imagen del código QR
 imagen_qr = codigo_qr.make_image(fill_color="black", back_color="white")
 
-# Guarda la imagen en un archivo
 imagen_qr.save("codigo_qr.png")
-
 print("Código QR generado y guardado como 'codigo_qr.png'")
